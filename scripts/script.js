119b194b21b8977ce07d331818731419f7c40060
Calvin <calvin.e.wetzel@gmail.com>
2024-02-10 18:23:15 -0500

    mobile-friendly nav bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,21 @@
-const hamMenu = document.querySelector('.ham-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu')
+// const hamMenu = document.querySelector('.ham-menu');
+// const offScreenMenu = document.querySelector('.off-screen-menu')
 
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active')
-})
+// hamMenu.addEventListener('click', () => {
+//     hamMenu.classList.toggle('active');
+//     offScreenMenu.classList.toggle('active')
+// })
+const navBarList = document.getElementById('navbar-list')
+
+
+function hamMenuActivate(){
+    console.log("fuck you");
+    if(navBarList.classList.contains('toggled')){
+        navBarList.classList.remove('toggled')
+        navBarList.classList.add('untoggled')
+    }
+    else{
+        navBarList.classList.remove('untoggled')
+        navBarList.classList.add('toggled')
+    }
+}
