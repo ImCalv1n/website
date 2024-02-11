183feccd2f09a9905999bb1a863899c2fe6a5bec
Calvin <calvin.e.wetzel@gmail.com>
2024-02-10 20:24:46 -0500

    added superlatives scroller, made superlatives size screen-friendly

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,5 @@
-// const hamMenu = document.querySelector('.ham-menu');
-// const offScreenMenu = document.querySelector('.off-screen-menu')
-
-// hamMenu.addEventListener('click', () => {
-//     hamMenu.classList.toggle('active');
-//     offScreenMenu.classList.toggle('active')
-// })
 const navBarList = document.getElementById('navbar-list')
-
+const scrollers = document.querySelectorAll('.scroller')
 
 function hamMenuActivate(){
     console.log("fuck you");
@@ -18,4 +11,23 @@ function hamMenuActivate(){
         navBarList.classList.remove('untoggled')
         navBarList.classList.add('toggled')
     }
+}
+
+if(!window.matchMedia('(prefers-reduced-motion: reduce)').matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach(scroller => {
+        scroller.setAttribute('data-animated', true);
+
+        const scrollerInner= scroller.querySelector('.scroller__inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute('aria-hidden', true);
+            scrollerInner.appendChild(duplicatedItem);
+        })
+    });
 }
